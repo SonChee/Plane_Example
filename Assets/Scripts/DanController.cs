3c083a5e245428753b4307dbca54f628293964dc
Son Che Dinh <che.dinh.son@framgia.com>
2015-01-28 13:57:07 +0700

    Report 1/2015

---
+++ Assets/Scripts/DanController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DanController : MonoBehaviour {
+	public float speed = 5;
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3(transform.position.x,transform.position.y + 1,transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.up * speed * Time.deltaTime);
+		if(transform.position.y > 6.2f){
+			Destroy(this.gameObject);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if(coll.gameObject.tag == "dich"){
+			Destroy(coll.gameObject);
+			Destroy(this.gameObject);
+		}
+	}
+}
